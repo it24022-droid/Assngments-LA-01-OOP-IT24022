50495c0c1cb1e644a2eff78860d47b26d3b1632c
it24022-droid <it24022@mbstu.ac.bd>
2026-04-26 11:58:14 +0600

    Update IT24022.java

--- IT24022.java
+++ IT24022.java
@@ -1,1 +1,9 @@
 
+public class IT24022 {
+    public static void main(String[] args) {
+            vehicle v = new vehicle(args[0],args[1]);
+            v.start();
+            vehicle v2 = new vehicle(args[0],args[1]);
+            v2.start();
+    }
+}
